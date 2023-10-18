dc9647e723565122c64cb7f0fe0af7532ee29055
ygouguass <ygouguasse@gmail.com>
2023-10-18 12:19:04 -0400

    first commit

---
+++ ex_jeuDeWinform/vues/Controles/LabelEvenement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_jeuDeWinform.vues.Controles
+{
+    public class LabelEvenement : Label
+    {
+        abstract void AbonnerEvenements();
+        
+        abstract void DesAbonnerEvenements();
+
+
+
+
+
+    }
+}
