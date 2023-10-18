38a2ad23723220dafa864b93a61264781968038b
ygouguass <ygouguasse@gmail.com>
2023-10-18 14:56:26 -0400

    6 commit

---
+++ ex_jeuDeWinform/vues/Controles/LabelJoueur.cs
@@ -0,0 +1,66 @@
+﻿using ex_jeuDeWinform.modeles;
+using ex_jeuDeWinform.vues.Controles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_jeuDeWinform.vues.controles
+{
+    public class LabelJoueur : LabelEvenement
+    {
+        private Joueur _joueur = new Joueur();
+
+        public Joueur Joueur { get => _joueur; set => ChagerJoueur(value);}
+
+        public LabelJoueur()
+        {
+            BorderStyle = BorderStyle.FixedSingle;
+            TextAlign= ContentAlignment.MiddleCenter;
+
+        }
+
+    
+        public void ChagerJoueur(Joueur value)
+        {
+            AbonnerEvenements();
+
+            _joueur = value;
+
+            DesAbonnerEvenements();
+
+            ActualiserAfficher();
+
+        }
+
+
+        public override void AbonnerEvenements()
+        {
+            _joueur.ChangementsSelectionne += _joueur_ChangementsSelectionne;
+
+        }
+
+        private void _joueur_ChangementsSelectionne(object? sender, EventArgs e)
+        {
+            ActualiserAfficher();
+        }
+
+        public override void DesAbonnerEvenements()
+        {
+            _joueur.ChangementsSelectionne-= _joueur_ChangementsSelectionne;
+
+        }
+
+
+        private void ActualiserAfficher()
+        {
+
+            Text=Joueur.ToString();
+
+        }
+
+
+
+    }
+}
---
+++ ex_jeuDeWinform/vues/Controles/LabelPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_jeuDeWinform.vues.controles
+{
+    public class LabelPoint : LabelPoint
+    {
+
+
+
+
+
+
+
+    }
+}
